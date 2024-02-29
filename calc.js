a13af8ca2289c3bd08ee24c23e3a3892a57fc550
joDis06 <jdisomma40@stuy.edu>
2024-02-29 14:52:03 -0500

    doing design for calc; can't figure out loop atm, do later

--- calc.js
+++ calc.js
@@ -2,7 +2,9 @@ import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 createApp({
     data() {
         return {
-            
+
+            tdData: "<td>a</td>"
+
         };
     },
 
