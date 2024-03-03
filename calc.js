0a22bc0f7da58262edf4fed1408537107c41efa9
jlin040 <jlin40@stuy.edu>
2024-03-03 13:16:01 -0500

    connected buttons.json to calc.js, parsing error for method calls

--- calc.js
+++ calc.js
@@ -1,14 +1,30 @@
 import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import buttonsArray from "./buttons.json" assert { type: "json" };
+
 const app = createApp({
     data() {
         return {
-            inputValue: ""
+            inputValue: "",
+            buttons: buttonsArray
         };
     },
     methods: {
+        handleInput(method) {
+            // let fn = window[method]
+            // fn();
+            console.log(this.buttons)
+            console.log(method)
+            console.log(typeof method)
+        },
         clear() {
             this.inputValue = ""
         },
+        delete() {
+            this.inputValue = this.inputvalue.slice(0, -1)
+        },
+        add(value) {
+            this.inputValue += value
+        }
     },
 
 })
