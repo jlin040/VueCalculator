3e10cebde9f679d4f2e5827ab8a838f358e1284a
joDis06 <jdisomma40@stuy.edu>
2024-03-06 14:13:53 -0500

    evaluation works; undo shows last expression; screen value updates when equals is pressed

--- calc.js
+++ calc.js
@@ -1,20 +1,68 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
-createApp({
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+const app = createApp({
     data() {
         return {
-
+            inputValue: "",
+            screenValue: "",
+            history: "",
+            lastUpdated: "",
         };
     },
-
-    computed: {
-
-    },
-
     methods: {
+        clear() {
+            this.inputValue = ""
+        },
+        undo() {
+            if (this.inputValue == "") return
+            this.inputValue = this.history
+        },
+        add(value) {
+            this.inputValue += value
+        },
+        calculate() {
+            console.log(this.inputValue)
+            let chars = [
+                {
+                    char: "×",
+                    replacement: "*"
+                },
 
+                {
+                    char: "√(",
+                    replacement: "Math.sqrt("
+                },
+
+                {
+                    char: "²",
+                    replacement: "** 2" 
+                }
+
+
+
+
+
+
+            ]
+            for (let c of chars) {
+                this.inputValue = this.inputValue.replaceAll(c["char"], c["replacement"])
+                this.lastUpdated = this.inputValue
+            }
+            this.inputValue = eval(this.inputValue)
+            this.screenValue = this.inputValue
+            
+        }
     },
-}).moount('#app')
 
+    watch: {
+        lastUpdated(value, oldValue) {
+            this.history = value
+        },
+    },
 
+})
 
-        
+const vm = app.mount('#app')
+window.app = vm
+
+    
